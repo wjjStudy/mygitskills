5ec781d23078f4bafb0114c6be6c09b7aeee2bcd
wjjStudy <13260597729@163.com>
2020-10-24 09:49:54 +0800

    修改

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
+import time
 
 class A:
     def spam(self):
@@ -8,6 +9,12 @@ class B(A):
         print('B.spam')
         super().spam() # Call parent spam()
 
-
 b = B()
 b.spam()
+
+# 获得当前时间时间戳time.time()
+#转换为其他日期格式,如:"%Y-%m-%d %H:%M:%S"
+timeArray = time.localtime(time.time())
+print(timeArray)
+otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+print(otherStyleTime)
